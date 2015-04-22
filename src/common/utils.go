1d9ca8e2e7beee02fb36fcb2633e81ab20db9ef5
zhangbaitong <zhangbaitong@163.com>
2015-04-22 15:10:08 +0800

    redis pool

--- src/common/utils.go
+++ src/common/utils.go
@@ -3,15 +3,20 @@ package common
 import (
 	"database/sql"
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/zhangbaitong/go-uuid/uuid"
 	"log"
 	"os"
+	"time"
 )
 
-//the global var of db connection,it will be create only once.
-var Conn *sql.DB
-var Logger *log.Logger
+//the global var of db connection,logger,redis pool.it will be create only once.
+var (
+	Conn   *sql.DB
+	Logger *log.Logger
+	pool   *redis.Pool
+)
 
 //Get db connection from mysql
 func GetDB() (db *sql.DB) {
@@ -39,4 +44,30 @@ func Log() *log.Logger {
 		Logger.Print("logger init success ...")
 	}
 	return Logger
+}
+
+//get redis connection pool
+func GetRedisPool() *redis.Pool {
+	if pool == nil {
+		pool = &redis.Pool{
+			MaxIdle:     3,
+			IdleTimeout: 240 * time.Second,
+			Dial: func() (redis.Conn, error) {
+				c, err := redis.Dial("tcp", "10.122.75.218:6379")
+				if err != nil {
+					return nil, err
+				}
+				// if _, err := c.Do("AUTH", password); err != nil {
+				//  c.Close()
+				//  return nil, err
+				// }
+				return c, err
+			},
+			TestOnBorrow: func(c redis.Conn, t time.Time) error {
+				_, err := c.Do("PING")
+				return err
+			},
+		}
+	}
+	return pool
 }
